ba8ff2b5225a67fa62c0fee405dfab8dbf11fb7a
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 13:56:28 +0100

    Enlevé des lignes de code de test.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿int firstNumber;
 int secondNumber;
-string firstString = "5.682";
-string secondString = "4.137";
-float flt1 = float.Parse(firstString);
-float flt2 = float.Parse(secondString);
 Console.WriteLine("Veuillez entrer la valeur du premier nombre : ");
 firstNumber = Int32.Parse(Console.ReadLine());
 Console.WriteLine("Veuillez entrer la valeur du deuxième nombre : ");
