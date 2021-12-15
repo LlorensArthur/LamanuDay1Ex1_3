d3deb794c338f981acd98a5ae7ec1e32a5fa4fdb
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 16:06:55 +0100

    Ajouté une sécurité sur les inputs utilisateurs (doit être un nombre)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿int firstNumber;
 int secondNumber;
 Console.WriteLine("Veuillez entrer la valeur du premier nombre : ");
-firstNumber = Int32.Parse(Console.ReadLine());
+while(!int.TryParse(Console.ReadLine(), out firstNumber)){
+    Console.WriteLine("Veuillez entrer uniquement un nombre.");
+}
 Console.WriteLine("Veuillez entrer la valeur du deuxième nombre : ");
-secondNumber = Int32.Parse(Console.ReadLine());
+while(!int.TryParse(Console.ReadLine(), out secondNumber)){
+    Console.WriteLine("Veuillez entrer uniquement un nombre.");
+}
 firstNumber += 33;
 secondNumber++;
 Console.WriteLine($"Le résultat de la division de {firstNumber} par {secondNumber} est : {(float)firstNumber / (float)secondNumber}");
